60d85f6285eccab721f2b25c33d7d4632e6f1dcb
Alan Silva <alan.silva_noesis@prestador.globo>
2024-01-17 16:13:09 -0300

    :zap:

---
+++ scripts/arch-kde.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Default
+sudo pacman -S curl git vim unzip ufw fwupd neofetch imagemagick ffmpeg fuse2
+sudo ufw enable
+
+# KDE
+sudo pacman -S okular gwenview kcalc vlc juk
+
+# Personal
+sudo pacman -S keepassxc vivaldi yt-dlp flameshot
+
+# Codex
+sudo pacman -S ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer
+
+# Printer
+sudo pacman -S cups avahi nss-mdns hplip print-manager system-config-printer
+sudo systemctl enable cups
+sudo systemctl enable avahi-daemon
+
+# Development
+sudo pacman -S dbus-glib # Lib para rodar o playwright
+
+# Work
+sudo pacman -S globalprotect-openconnect
+
+# Pending
+## Paru
+#paru -S ccrypt
+
+## /opt
+# rustrover
+
+
