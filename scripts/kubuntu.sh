ea6294cdf2c4be8a93dadd72cb6d155b72b0ae19
Alan <alan.silva_noesis@prestador.globo>
2024-01-25 21:39:03 -0300

    :zap:

---
+++ scripts/kubuntu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Updates
+sudo apt update && sudo apt upgrade
+
+# Programs
+sudo apt install keepassxc vim git curl
+
+# Rust
+sudo apt install build-essential libssl-dev pkg-config
+
+# Git
+git config --global credential.helper store
+
+# Dolphin
+# Konsole
+# Dark theme
