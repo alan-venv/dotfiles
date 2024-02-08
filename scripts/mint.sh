cc67f2c1a73eda61b72dd5059e8c52ea522665d1
Alan <alan.silva_noesis@prestador.globo>
2024-02-08 01:12:36 -0300

    :zap:

---
+++ scripts/mint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Purge some shits
+#sudo apt purge libreoffice*
+#sudo rm -r /etc/libreoffice/
+sudo apt purge firefox
+sudo apt purge java*
+
+# Packages
+sudo apt install vim git curl ccrypt
+sudo apt install build-essential libssl-dev
+
+# Flatpak
+flatpak install -y flathub org.mozilla.firefox
+flatpak install -y flathub org.keepassxc.KeePassXC
+flatpak install -y flathub com.getpostman.Postman
+flatpak install -y flathub com.vivaldi.Vivaldi
+
+# Shortcuts
+# - Switch to workspace (ALT + NUM)
+# - Launch terminal (ALT + RETURN)
+# - Lock screen (ALT + L)
+# - Poweroff (ALT + P)
