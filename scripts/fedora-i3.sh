cd30685ac3148e2f49eebd5853ea0471faa5d7dd
Alan Silva <alan.silva_noesis@prestador.globo>
2024-02-28 17:45:41 -0300

    :zap:

--- scripts/fedora-i3.sh
+++ scripts/fedora-i3.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
 # liveinst
 
 sudo dnf update
 sudo dnf upgrade
 sudo dnf install firefox keepassxc okular ufw git vim rofi neofetch lxappearance arc-theme picom feh
 
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable docker
+sudo systemctl start docker
 
