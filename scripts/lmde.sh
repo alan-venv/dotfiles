53c3a6b49a5adea4e27196a4f4dd887e4bf84c06
Alan Silva <42683597+alan-venv@users.noreply.github.com>
2024-05-07 15:10:31 -0300

    :zap:

--- scripts/lmde.sh
+++ scripts/lmde.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
-# Purge some shits
-sudo apt purge libreoffice*
+# PURGE SOME SHITS
+sudo apt purge -y libreoffice*
 sudo rm -r /etc/libreoffice/
-sudo apt purge firefox
-sudo apt purge java*
+sudo apt purge -y firefox
+sudo apt purge -y java*
 
-# Packages
-sudo apt install vim git curl ccrypt
-sudo apt install build-essential libssl-dev
+# PACKAGES
+sudo apt update && sudo apt upgrade
+sudo apt install -y vim git curl unzip ccrypt ufw flatpak podman
+sudo apt install -y build-essential libssl-dev
+sudo apt install -y flameshot
 
-# Flatpak
+# FLATPAKS
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y flathub org.mozilla.firefox
 flatpak install -y flathub org.keepassxc.KeePassXC
-flatpak install -y flathub com.getpostman.Postman
-flatpak install flathub com.vivaldi.Vivaldi
+flatpak install -y flathub io.github.ungoogled_software.ungoogled_chromium
 
-# Shortcuts
-- Switch to workspace (ALT + NUM)
-- Launch terminal (ALT + RETURN)
-- Lock screen (ALT + L)
+# CONFIGS
+sudo ufw enable
+git config --global credential.helper store
+
+# SHORTCUTS
+# - Switch to workspace (ALT + NUM)
+# - Launch terminal (ALT + RETURN)
+# - Lock screen (ALT + L)
