41a3930aa08b9ca10ff42aad8a7c5c40eab5faaa
Alan <alan.silva_noesis@prestador.globo>
2025-12-22 23:05:26 -0300

    wip

--- scripts/os/mint.sh
+++ scripts/os/mint.sh
@@ -29,6 +29,24 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # ZED
 curl -f https://zed.dev/install.sh | sh
 
+# DOCKER
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: trixie
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable --now docker
+sudo usermod -aG docker $USER
+
 # SHORTCUTS
 # - switch to workspace (ALT + NUM)
 # - launch terminal (ALT + RETURN)
