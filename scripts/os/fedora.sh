aefbab8d22f4af8b466f351e4778113cf3430ed1
Alan <alan.silva_noesis@prestador.globo>
2026-05-19 11:44:01 -0300

    wip

---
+++ scripts/os/fedora.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo dnf install git vim htop keepassxc ccrypt fastfetch
+sudo dnf install gcc openssl-devel
+sudo dnf install ripgrep jq xxd
+sudo dnf remove kontact akregator kmail neochat korganizer kaddressbook kwalletmanager5
+sudo dnf remove java-*-openjdk-*
+
+## OTHERS
+# - [ZED](https://zed.dev)
+# - [RUST](https://www.rust-lang.org/tools/install)
+# - [CHROME](https://www.google.com/chrome)
+# - [DOCKER](https://docs.docker.com/engine/install/fedora)
+
+## Docker Post Install
+# $ sudo usermod -aG docker $USER && reboot
