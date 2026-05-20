556225fa6f2058e4ad90d633e2c53336e0a9a985
Alan <alan.silva_noesis@prestador.globo>
2026-05-20 13:55:41 -0300

    wip

--- scripts/os/fedora.sh
+++ scripts/os/fedora.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo dnf install git vim htop keepassxc ccrypt fastfetch
+sudo dnf install git vim htop alacritty keepassxc ccrypt fastfetch
 sudo dnf install gcc openssl-devel
 sudo dnf install ripgrep jq xxd
 sudo dnf remove kontact akregator kmail neochat korganizer kaddressbook kwalletmanager5
